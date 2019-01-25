4ef7b200b10bd41f04f9d37d91201d552436fb94
moroztaras <moroztaras@i.ua>
2019-01-25 20:14:25 +0200

    Security

--- src/Security/ApiAuthenticator.php
+++ src/Security/ApiAuthenticator.php
@@ -13,9 +13,11 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-class TokenAuthenticator extends AbstractGuardAuthenticator
+class ApiAuthenticator extends AbstractGuardAuthenticator
 {
     private $em;
+
+    const X_API_KEY = 'x-api-key';
 
     public function __construct(EntityManagerInterface $em)
     {
@@ -39,7 +41,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
     public function getCredentials(Request $request)
     {
         return [
-          'token' => $request->headers->get('X-AUTH-TOKEN'),
+            'token' => $request->headers->get('X-AUTH-TOKEN'),
         ];
     }
 
@@ -53,7 +55,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
 
         // if a User object, checkCredentials() is called
         return $this->em->getRepository(User::class)
-          ->findOneBy(['apiToken' => $apiToken]);
+            ->findOneBy(['apiToken' => $apiToken]);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
@@ -74,7 +76,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         $data = [
-          'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
 
             // or to translate this message
             // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
@@ -84,13 +86,13 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
     }
 
     /**
-     * Called when authentication is needed, but it's not sent.
+     * Called when authentication is needed, but it's not sent
      */
     public function start(Request $request, AuthenticationException $authException = null)
     {
         $data = [
             // you might translate this message
-          'message' => 'Authentication Required',
+            'message' => 'Authentication Required'
         ];
 
         return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
